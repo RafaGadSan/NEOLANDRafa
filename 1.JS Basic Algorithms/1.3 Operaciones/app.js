397968c776ca35631748700bc98421ce53ecce5a
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-04 15:33:54 +0200

    1.JS Basic Algorithms

---
+++ 1.JS Basic Algorithms/1.3 Operaciones/app.js
@@ -0,0 +1,11 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+//1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+//1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+/*1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.*/
+
+/*1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.*/
