9b7ca394cd36b49a816416cd9f2c54c11a04bb14
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-05 22:53:37 +0200

    cd

---
+++ 3.Loops/3. Probando For...of/app.js
@@ -0,0 +1,17 @@
+/*Usa un bucle forof para recorrer todos los destinos del array.
+ Imprime en un ***console.log*** sus valores.
+
+Puedes usar este array:
+ */
+
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+for (let valor of placesToTravel) {
+  console.log(valor);
+}
