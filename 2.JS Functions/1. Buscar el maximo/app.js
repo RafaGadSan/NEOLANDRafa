40b88b007ff71a5d4e0ceaafd9a7f81053fbf599
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-05 15:28:28 +0200

    1.JS Functions/

---
+++ 2.JS Functions/1. Buscar el maximo/app.js
@@ -0,0 +1,5 @@
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+function sum(numberOne, numberTwo) {
+  return Math.max(numberOne, numberTwo);
+}
+console.log(sum(1, 5));
---
+++ 2.JS Functions/2. Buscar la palabra mas larga/app.js
@@ -0,0 +1,18 @@
+/*Completa la función que tomando un array de strings como argumento devuelva el más largo,
+ en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función:*/
+
+const avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(param) {
+  let posicion = 0;
+  let longitud = 0;
+  for (let index = 0; index < param.length; index++) {}
+}
