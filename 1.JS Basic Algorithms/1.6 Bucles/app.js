962ac8e58a2b9d7193857a0a2c2c9f33a819fbec
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-05 14:40:53 +0200

    1.JS Basic Algorithms/

--- 1.JS Basic Algorithms/1.6 Bucles/app.js
+++ 1.JS Basic Algorithms/1.6 Bucles/app.js
@@ -6,7 +6,7 @@ console.log("-------------");
 /*1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
 cuando el resto del numero dividido entre 2 sea 0.*/
 for (let i = 0; i < 10; i++) {
-  if (i % 2 === 0) console.log(i);
+  i % 2 == 0 && console.log(i);
 }
 console.log("-------------");
 /*1.3 Crea un bucle para conseguir dormir contando ovejas. 
