98c10d31ad7bf9537323fbf0ef2247f8463e3a94
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-05 18:13:19 +0200

    1.JS Functions/

---
+++ 2.JS Functions/8. Contador de repeticiones/app.js
@@ -0,0 +1,18 @@
+/*Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.
+  Puedes usar este array para probar tu función:*/
+const counterWords = [
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "upgrade",
+  "code",
+];
+function repeatCounter(param) {
+  // insert code
+}
