1669d4269f412d0ad772f31ed40095b4182737d6
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-06 22:29:46 +0200

    commit

--- 4. JS Review Basics/6.Funcion swap/app.js
+++ 4. JS Review Basics/6.Funcion swap/app.js
@@ -4,4 +4,25 @@
 
 Sugerencia de array:*/
 
-["Mesirve", "Cristiano Romualdo", "Fernando Muralla", "Ronalguiño"];
+const jugadores = [
+  "Mesirve",
+  "Cristiano Romualdo",
+  "Fernando Muralla",
+  "Ronalguiño",
+];
+
+const swap = (array, indice1, indice2) => {
+  let palabra1;
+  let palabra2;
+  array.forEach((element, index) => {
+    index == indice1 && (palabra1 = element);
+    index == indice2 && (palabra2 = element);
+  });
+
+  array[indice2] = palabra1;
+  array[indice1] = palabra2;
+
+  return array;
+};
+console.log(`El array original es ${jugadores}`);
+console.log(`El array intercambiado es ${swap(jugadores, 1, 2)}`);
