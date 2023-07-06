1e2806a6a20d9122b157d11b3d2496326ac79996
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-06 22:00:59 +0200

    commit

---
+++ 4. JS Review Basics/6.Funcion swap/app.js
@@ -0,0 +1,7 @@
+/*Crea una función llamada `swap()` que reciba un array y dos parametros que sean
+ indices del array. La función deberá intercambiar la posición de los valores de 
+ los indices que hayamos enviado como parametro. Retorna el array resultante.
+
+Sugerencia de array:*/
+
+["Mesirve", "Cristiano Romualdo", "Fernando Muralla", "Ronalguiño"];
