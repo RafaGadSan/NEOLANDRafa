22d6f47083958f4a73e2be10a115a0442fea1bfe
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-07 17:48:01 +0200

    hecha la parte 5 de Filter

--- JS/8. JS ES6/5. Filter/app.js
+++ JS/8. JS ES6/5. Filter/app.js
@@ -29,22 +29,40 @@ const nuevoStreamers1 = streamers1.filter(
 );
 console.log(nuevoStreamers1);
 
+//!------------------------5.3------------------------
+console.log("\n------------------------5.3------------------------");
 // 5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array
 // con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos
 // usar la funcion .includes() para la comprobación.
-// const streamers = [
-// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-// ];
 
+const streamers2 = [
+  { name2: "Rubius", age2: 32, gameMorePlayed2: "Minecraft" },
+  { name2: "Ibai", age2: 25, gameMorePlayed2: "League of Legends" },
+  { name2: "Reven", age2: 43, gameMorePlayed2: "League of Legends" },
+  { name2: "AuronPlay", age2: 33, gameMorePlayed2: "Among Us" },
+];
+
+const filtroStreamers2 = streamers2.filter(
+  (i) => i.name2.includes("u") && (i = i)
+);
+console.log(filtroStreamers2);
+
+//!------------------------5.3------------------------
+console.log("\n------------------------5.3------------------------");
 // 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan
 // el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion
 // .includes() para la comprobación.
 // Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando
 // .age sea mayor que 35.
+const filtro2Streamers2 = streamers2.filter(
+  (i) =>
+    i.gameMorePlayed2.includes("Legends") &&
+    (i.age2 > 35
+      ? (i.gameMorePlayed2 = i.gameMorePlayed2.toUpperCase())
+      : (i = i))
+);
 
+console.log(filtro2Streamers2);
 // 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola
 // los streamers que incluyan la palabra introducida en el input. De esta forma, si
 // introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
