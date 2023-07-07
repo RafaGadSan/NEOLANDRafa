b5659eb64e86529f960d9caaf9707cb74c7d9a9f
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-07 16:12:34 +0200

    Todo hecho de Spread Operator, pero repasar el 3.5

--- JS/8. JS ES6/3. Spread Operator/app.js
+++ JS/8. JS ES6/3. Spread Operator/app.js
@@ -41,8 +41,17 @@ console.log(
   )} es:`
 );
 console.log(toy2Completo);
+
 //!----------------------------3.5---------------------------
 console.log("----------------------------3.5---------------------------");
 /*3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
 pero sin editar el array inicial. De nuevo, usando spread operatos.*/
 const colors = ["rojo", "azul", "amarillo", "verde", "naranja"];
+
+//?HAY ALGUNA MANERA DE HACER const copiaColors = [...colors].splice(2, 1); Y QUE NO ME ALMACENE SOLO LA POSICIÓN 2?
+
+const copiaColors = [...colors];
+copiaColors.splice(2, 1);
+console.log(
+  `El array original es ${colors}\nEl array modificado es ${copiaColors}`
+);
