d72337c0b743ffd4bc6f93efffd05b552d779f94
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-08 20:19:09 +0200

    acabado el ejercicio de Find

--- JS/8. JS ES6/6. Find/app.js
+++ JS/8. JS ES6/6. Find/app.js
@@ -18,19 +18,44 @@ const movies = [
 movies.find((peli) => peli.date === 2010)
   ? console.log(`SÍ hay pelis del 2010`)
   : console.log(`NO hay pelis del 2010`);
+
 //!------------------------6.3------------------------
 console.log("\n------------------------6.3------------------------");
 // 6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre
 // 'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa
 // spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación
 // lo queremos meter en la propiedad .mutation del objeto fusionado.
-// const aliens = [
-// 	{name: 'Zalamero', planet: 'Eden', age: 4029},
-// 	{name: 'Paktu', planet: 'Andromeda', age: 32},
-// 	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
-// ];
-// const mutations = [
-// 	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
-// 	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
-// 	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
-// ];
+const aliens = [
+  { name: "Zalamero", planet: "Eden", age: 4029 },
+  { name: "Paktu", planet: "Andromeda", age: 32 },
+  { name: "Cucushumushu", planet: "Marte", age: 503021 },
+];
+const mutations = [
+  {
+    name: "Porompompero",
+    description:
+      "Hace que el alien pueda adquirir la habilidad de tocar el tambor",
+  },
+  {
+    name: "Fly me to the moon",
+    description: "Permite volar, solo y exclusivamente a la luna",
+  },
+  {
+    name: "Andando que es gerundio",
+    description: "Invoca a un señor mayor como Personal Trainer",
+  },
+];
+//?antes tenía hecho ésto y en los spread operators usaba las variables alienCucu y mutPorom.
+// let alienCucu;
+// let mutPorom;
+// alienCucu = aliens.find((alien) => alien.name == "Cucushumushu");
+// console.log(alienCucu);
+// mutPorom = mutations.find((mutation) => mutation.name == "Porompompero");
+// console.log(mutPorom);
+const megaAlien = {
+  ...aliens.find((alien) => alien.name == "Cucushumushu"),
+  mutacion: {
+    ...mutations.find((mutation) => mutation.name == "Porompompero"),
+  },
+};
+console.log(megaAlien);
