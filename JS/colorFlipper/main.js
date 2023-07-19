6c6e9a756cbed2b50e1e6f8518c708c4d14fcda2
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-19 17:37:25 +0200

    color Flipper copiado y entendido

---
+++ JS/colorFlipper/main.js
@@ -0,0 +1,34 @@
+import "./style.css";
+
+const COLOR_PALETTE = {
+  "#28262C": "Raisin Black",
+  "#998FC7": "Blue Bell",
+  "#D4C2FC": "Lavender Blue",
+  "#F9F5FF": "Magnolia",
+  "#14248A": "Resolution Blue",
+};
+
+const addOptionsToColorPicker = () => {
+  const colorPickerSelect = document.querySelector("#color-picker");
+  Object.keys(COLOR_PALETTE).forEach((color) => {
+    const option = document.createElement("option");
+    option.value = color;
+    option.innerText = COLOR_PALETTE[color];
+    colorPickerSelect.append(option);
+  });
+};
+
+const addEventListenerToColorPicker = () => {
+  const colorPickerSelect = document.querySelector("#color-picker");
+  const colorName = document.querySelector("#color-name");
+  colorPickerSelect.addEventListener("change", (event) => {
+    const newColor = event.target.value;
+    document.body.style.backgroundColor = newColor;
+    //Le asignamos a nuestro h2 el valor de la clave que hay almacenada en newColor al igual que hemos hecho en la primera función
+    const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`;
+    colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : "-";
+  });
+};
+
+addOptionsToColorPicker();
+addEventListenerToColorPicker();
