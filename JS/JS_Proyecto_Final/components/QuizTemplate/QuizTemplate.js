93c8b24bcbe146c88aa620752afb24741eee2a68
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-07-30 00:04:33 +0200

    ahorcado acabado del todo

--- JS/JS_Proyecto_Final/components/QuizTemplate/QuizTemplate.js
+++ JS/JS_Proyecto_Final/components/QuizTemplate/QuizTemplate.js
@@ -1,80 +1,14 @@
 export const template = () => ` <div class="container">
-  <section id="p0">
-    <h2>1 - el primer juego de arcade salió en 1971. ¿Cómo se llamaba?</h2>
-    <label>
-      <input class="entrada" type="radio" value="1" name="p0" />Pong</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="2" name="p0" />Computer Space</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="3" name="p0" />Galaxian</label
-    >
-  </section>
-  <section id="p1">
-    <h2>2 - ¿Cuál de estos videojuegos está ampliamente considerado como el más vendido de todos los tiempos?</h2>
-    <label>
-      <input class="entrada" type="radio" value="1" name="p1" />Minecraft</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="2" name="p1" />Super Mario Bros.</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="3" name="p1" />Grand Theft Auto</label
-    >
-  </section>
-  <section id="p2">
-    <h2>3 - La consola de videojuegos más vendida de todos los tiempos es PlayStation 2 de Sony. También fue la primera consola de juegos dotada de...</h2>
-    <label>
-      <input class="entrada" type="radio" value="1" name="p2" />Conexión a internet</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="2" name="p2" />Controles inalámbricos</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="3" name="p2" />Un reproductor de DVD incorporado</label
-    >
-  </section>
-  <section id="p3">
-    <h2>4 - Dejando de lado su prohibición en Alemania por su uso de iconografía nazi, Wolfenstein 3D es famoso por fundar, ¿qué género de videojuegos?</h2>
-    <label>
-      <input
-        class="entrada"
-        type="radio"
-        value="1"
-        name="p3"
-      />Beat'em up</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="2" name="p3" />Shooter de disparo en primera persona</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="3" name="p3" />Battle royale</label
-    >
-  </section>
-  <section id="p4">
-    <h2>5 - A cualquiera que haya jugado Tetris se le habrá quedado grabada en la cabeza la melodía, aunque solo sea una vez. ¿En qué está basado el tema de la canción?</h2>
-    <label>
-      <input class="entrada" type="radio" value="1" name="p4" />Una canción folclórica rusa</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="2" name="p4" />Una canción de juerga alemana</label
-    >
-    <label>
-      <input class="entrada" type="radio" value="3" name="p4" />Una canción de amor francesa</label
-    >
-  </section>
-  <section id="p5">
-  <h2>5 - ¿Los juegos de smartphone cuentan como videojuegos? Por supuesto que sí. ¿Cuál es el juego de celular que más veces se ha descargado de todos los tiempos?</h2>
-  <label>
-    <input class="entrada" type="radio" value="1" name="p5" />Pokemon Go</label
-  >
-  <label>
-    <input class="entrada" type="radio" value="2" name="p5" />Angry Birds</label
-  >
-  <label>
-    <input class="entrada" type="radio" value="3" name="p5" />Candy Crush</label
-  >
+<section id="p0"> 
+</section>
+<section id="p1"> 
+</section>
+<section id="p2"> 
+</section>
+<section id="p3"> 
+</section>
+<section id="p4"> 
+</section> 
 </section>
   <button id="bCorregir">CORREGIR</button>
   <h2>Cantidades acertadas: <span id="resultado"></span></h2>
--- JS/JS_Proyecto_Final/pages/Quiz/Quiz.js
+++ JS/JS_Proyecto_Final/pages/Quiz/Quiz.js
@@ -3,13 +3,61 @@ import "./Quiz.css";
 
 export const printTemplateQuiz = () => {
   document.querySelector("main").innerHTML = template();
-  addEventListeners();
+  const preguntas = fetch("./pages/Quiz/preguntas.json")
+    .then((response) => response.json())
+    .then((preguntas) => addEventListeners(preguntas["preguntas"]))
+    .catch((error) => console.error("Error:", error));
 };
 
-//Los value correctos del input  son estos.
-let correctas = [2, 1, 3, 2, 1, 1];
+function generarNumerosAleatorios(n, numpreguntasmaximas) {
+  let numeros = new Set();
 
-const addEventListeners = () => {
+  while (numeros.size < n) {
+    let numAleatorio = Math.floor(Math.random() * numpreguntasmaximas) + 1;
+    if (numAleatorio in numeros) {
+      continue;
+    } else {
+      numeros.add(numAleatorio);
+    }
+  }
+
+  return [...numeros];
+}
+const addEventListeners = (preguntas) => {
+  let numPreguntas = 5;
+  let preguntasyrespuestascorrectas = [];
+  console.log(preguntas);
+  let preguntasSeleccionadas = generarNumerosAleatorios(
+    numPreguntas,
+    preguntas.length - 1
+  );
+  let count = 0;
+  for (let numeroPregunta of preguntasSeleccionadas) {
+    console.log(preguntas[numeroPregunta]);
+    const pregunta = preguntas[numeroPregunta]["pregunta"];
+    const respuestas = preguntas[numeroPregunta]["opciones"];
+    preguntasyrespuestascorrectas.push({
+      pregunta: pregunta,
+      respuestacorrecta: respuestas[0],
+    });
+    respuestas.sort(() => Math.random() - 0.5);
+
+    const section = document.getElementById("p" + count);
+    count = count + 1;
+    section.innerHTML = `
+    <div>
+    <h2>${count} - ${pregunta}</h2>
+    <label>
+      <input class="entrada" type="radio" value="${respuestas[0]}" name="p${count}" />${respuestas[0]}</label>
+    <label>
+      <input class="entrada" type="radio" value="${respuestas[1]}" name="p${count}" />${respuestas[1]}
+      </label>
+    <label>
+      <input class="entrada" type="radio" value="${respuestas[2]}" name="p${count}" />${respuestas[2]}</label>
+    </div>
+    `;
+  }
+
   const boton = document.getElementById("bCorregir");
   boton.addEventListener("click", () => {
     //Me traigo todas las secciones
@@ -18,7 +66,12 @@ const addEventListeners = () => {
     secciones.forEach((seccion, index) => {
       const inputs = seccion.querySelectorAll("input");
       inputs.forEach((input) => {
-        if (input.checked && Number(input.value) === correctas[index]) {
+        if (
+          input.checked &&
+          input.value != undefined &&
+          input.value ===
+            preguntasyrespuestascorrectas[index]["respuestacorrecta"]
+        ) {
           respuestasUsuario.push("correcto");
         }
       });
