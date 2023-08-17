b7cf5ff241d23aa6a9ded263557c69401a446ce9
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-08-17 18:23:44 +0200

    casi miniproyecto3

---
+++ REACT/miniProyectoReactJS[3]/miniproyecto3/src/components/Read.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Author = () => {};
+const Books = () => {};
+
+const Read = () => {
+  return (
+    <div>
+      <h2>Libros que he leído</h2>
+      <Author />
+      <Books />
+    </div>
+  );
+};
+
+export default Read;
