6a394df122dbedcfb81a4f6e58d9dd53d9aec422
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-08-17 18:23:29 +0200

    casi miniproyecto3

---
+++ REACT/miniProyectoReactJS[3]/miniproyecto3/src/HOBBIES/HOBBIES.js
@@ -0,0 +1,71 @@
+export const HOBBIES = {
+  read: {
+    title: "To Kill a Mockingbird",
+    authorName: "Harper",
+    authorSurname: "Lee",
+    genre: "Novel",
+    dateOfPublication: "11/07/1960",
+    authorBirthDate: "28/04/1926",
+    bookImage: "url-to-image",
+    otherBooks: [
+      {
+        info: "Go Set a Watchman",
+      },
+      {
+        info: "Mockingbird Songs: My Friendship with Harper Lee",
+      },
+      {
+        info: "In Cold Blood",
+      },
+      {
+        info: "The Great Gatsby",
+      },
+    ],
+  },
+  sports: [
+    {
+      name: "Football",
+      indoor: false,
+      favoriteTeam: "Barcelona",
+    },
+    {
+      name: "Basketball",
+      indoor: true,
+      favoriteTeam: "Los Angeles Lakers",
+    },
+    {
+      name: "Tennis",
+      indoor: false,
+      favoriteTeam: "",
+    },
+  ],
+  movies: [
+    {
+      name: "Inception",
+      type: "Movie",
+      genre: "Science Fiction",
+      vote: 9,
+    },
+    {
+      name: "Stranger Things",
+      type: "Series",
+      genre: "Science Fiction, Horror",
+      vote: 8,
+    },
+  ],
+  languages: {
+    language: "English",
+    wrlevel: "Advanced",
+    splevel: "Intermediate",
+  },
+  songsHeard: [
+    "Bohemian Rhapsody - Queen",
+    "Hotel California - Eagles",
+    "Shape of You - Ed Sheeran",
+    "Imagine - John Lennon",
+    "Stairway to Heaven - Led Zeppelin",
+    "Billie Jean - Michael Jackson",
+    "Rolling in the Deep - Adele",
+    "Hey Jude - The Beatles",
+  ],
+};
