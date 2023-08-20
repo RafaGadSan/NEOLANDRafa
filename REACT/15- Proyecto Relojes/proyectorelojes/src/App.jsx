ec8608872c54c6b922458b217060fb8f094d74d3
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-08-20 17:16:18 +0200

    Acabado proyecto reloj sin estilos

---
+++ REACT/15- Proyecto Relojes/proyectorelojes/src/App.jsx
@@ -0,0 +1,15 @@
+import DigitalClock from "./components/DigitalClock";
+import Countdown from "./components/Countdown";
+import Stopwatch from "./components/Stopwatch";
+
+function App() {
+  return (
+    <>
+      <DigitalClock />
+      <Countdown />
+      <Stopwatch />
+    </>
+  );
+}
+
+export default App;
